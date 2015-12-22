c8b4d158ee7cdb5275675a4eab628a2a4d07efea
Przemek Lal <przemek.lal@gmail.com>
2015-12-22 18:18:24 +0100

    Utworzenie serwera z połączeniem TCP na porcie 1357 i inicjalizacja obiektu Game

---
+++ Game/Server/Server.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RemotableObjects;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+using System.Runtime.Remoting;
+
+
+namespace Server
+{
+    class Server
+    {
+        Game game;
+
+        Server()
+        {
+            game = Game.Instance;
+        }
+
+        static void Main(string[] args)
+        {
+            TcpChannel channel = new TcpChannel(1357);
+            ChannelServices.RegisterChannel(channel, false);
+            RemotingConfiguration.RegisterWellKnownServiceType(typeof(Game), "Game", WellKnownObjectMode.Singleton);
+            Console.WriteLine("Game server up and running. Waiting for clients...");
+
+            while (true)
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
